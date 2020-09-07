7910e68c15421f78647f8ee2bda7f4b88051f057
Seo hyunh ho <hyunh84@hanmail.net>
2020-09-07 21:21:45 +0900

    Updates

---
+++ precache-manifest.4f4eb31a1396ee997627ff580601c5f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f144f79468775d3f904e77daa37709f",
+    "url": "/clonflix/index.html"
+  },
+  {
+    "revision": "cff56c5c1fc028d9aa3f",
+    "url": "/clonflix/static/js/2.715dce63.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/clonflix/static/js/2.715dce63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdba7232860045f4f059",
+    "url": "/clonflix/static/js/main.3e64e3cf.chunk.js"
+  },
+  {
+    "revision": "ffd30d023ebdabb2a564",
+    "url": "/clonflix/static/js/runtime-main.c9038d43.js"
+  }
+]);
